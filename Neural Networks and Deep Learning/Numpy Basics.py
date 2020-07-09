42fa0fe54086e2036ca30a92574b62fb984557c3
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-07-09 19:44:17 +0530

    Create Numpy Basics.py

---
+++ Neural Networks and Deep Learning/Numpy Basics.py
@@ -0,0 +1,57 @@
+# GRADED FUNCTION: basic_sigmoid
+
+import math
+
+def basic_sigmoid(x):
+    """
+    Compute sigmoid of x.
+
+    Arguments:
+    x -- A scalar
+
+    Return:
+    s -- sigmoid(x)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1/(1+math.exp(-x))
+    ### END CODE HERE ###
+    
+    return s
+    
+# GRADED FUNCTION: sigmoid_derivative
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+    
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+    
+    ### START CODE HERE ### (≈ 2 lines of code)
+    s = sigmoid(x)
+    ds = s*(1-s)
+    ### END CODE HERE ###
+    
+    return ds
+  
+# GRADED FUNCTION: image2vector
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+    
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    v = image.reshape((image.shape[0]*image.shape[1]*image.shape[2],1))
+    ### END CODE HERE ###
+    
+    return v
