f9c899f9fdbf2fbb649a126870dbcc5ebd84660c
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-07-11 23:15:57 +0530

    Update Cat_LR_NN.py

--- Cat_LR_NN.py
+++ Cat_LR_NN.py
@@ -30,3 +30,19 @@ print ("train_set_x shape: " + str(train_set_x_orig.shape))
 print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
+
+# Reshape the training and test examples
+
+### START CODE HERE ### (≈ 2 lines of code)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+### END CODE HERE ###
+
+print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
