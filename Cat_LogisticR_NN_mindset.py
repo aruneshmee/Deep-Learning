0f62e82876db1a4def41b698147c5208a33489d4
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-07-15 23:08:13 +0530

    Update Cat_LogisticR_NN_mindset.py

--- Cat_LogisticR_NN_mindset.py
+++ Cat_LogisticR_NN_mindset.py
@@ -313,3 +313,8 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate
     return d
 
 d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+# Example of a picture that was wrongly classified.
+index = 1
+plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
