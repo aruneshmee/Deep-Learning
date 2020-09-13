cb2cceda4d521597b953b94a55373f0ce90ce090
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-09-13 21:41:06 +0530

    Update Helper_functions.py

--- Convolution Neural Networks/Helper_functions.py
+++ Convolution Neural Networks/Helper_functions.py
@@ -31,4 +31,42 @@ def zero_pad(X, pad):
     
     return X_pad
     
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =\n", x.shape)
+print ("x_pad.shape =\n", x_pad.shape)
+print ("x[1,1] =\n", x[1,1])
+print ("x_pad[1,1] =\n", x_pad[1,1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+    of the previous layer.
+    
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+    
+    Returns:
+    Z -- a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+
+    ### START CODE HERE ### (≈ 2 lines of code)
+    # Element-wise product between a_slice_prev and W. Do not add the bias yet.
+    s = np.multiply(a_slice_prev, W)
+    # Sum over all entries of the volume s.
+    Z = np.sum(s)
+    # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+    Z = Z + float(b)
+    ### END CODE HERE ###
+
+    return Z
     
