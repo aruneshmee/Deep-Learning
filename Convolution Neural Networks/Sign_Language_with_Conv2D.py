f92810e137553cf47d84028c3e9fd79d56e2f642
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-09-14 11:16:23 +0530

    Update Sign_Language_with_Conv2D.py

--- Convolution Neural Networks/Sign_Language_with_Conv2D.py
+++ Convolution Neural Networks/Sign_Language_with_Conv2D.py
@@ -80,3 +80,59 @@ def initialize_parameters():
                   "W2": W2}
     
     return parameters
+
+tf.reset_default_graph()
+with tf.Session() as sess_test:
+    parameters = initialize_parameters()
+    init = tf.global_variables_initializer()
+    sess_test.run(init)
+    print("W1[1,1,1] = \n" + str(parameters["W1"].eval()[1,1,1]))
+    print("W1.shape: " + str(parameters["W1"].shape))
+    print("\n")
+    print("W2[1,1,1] = \n" + str(parameters["W2"].eval()[1,1,1]))
+    print("W2.shape: " + str(parameters["W2"].shape))
+    
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+    
+    Note that for simplicity and grading purposes, we'll hard-code some values
+    such as the stride and kernel (filter) sizes. 
+    Normally, functions should take these values as function parameters.
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "W2"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+    
+    ### START CODE HERE ###
+    # CONV2D: stride of 1, padding 'SAME'
+    Z1 = tf.nn.conv2d(X,W1, strides = [1,1,1,1], padding = 'SAME')
+    # RELU
+    A1 = tf.nn.relu(Z1)
+    # MAXPOOL: window 8x8, stride 8, padding 'SAME'
+    P1 = tf.nn.max_pool(A1, ksize = [1,8,8,1], strides = [1,8,8,1], padding = 'SAME')
+    # CONV2D: filters W2, stride 1, padding 'SAME'
+    Z2 = tf.nn.conv2d(P1, W2, strides = [1,1,1,1], padding = 'SAME')
+    # RELU
+    A2 = tf.nn.relu(Z2)
+    # MAXPOOL: window 4x4, stride 4, padding 'SAME'
+    P2 = tf.nn.max_pool(A2, ksize = [1,4,4,1], strides = [1,4,4,1], padding = 'SAME')
+    # FLATTEN
+    F = tf.contrib.layers.flatten(P2)
+    # FULLY-CONNECTED without non-linear activation function (not not call softmax).
+    # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None" 
+    Z3 = tf.contrib.layers.fully_connected(F, 6, activation_fn=None)
+    ### END CODE HERE ###
+
+    return Z3
+
